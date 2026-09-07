224c50b5c644fc03058fa3399d5b8dff54f27e65
shishiqiqi <tan_shi_qi_3@students.edu.sg>
2026-09-07 14:02:36 +0800

    Initial Commit

---
+++ challenge2/challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge2App.swift
+//  challenge2
+//
+//  Created by T Krobot on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
